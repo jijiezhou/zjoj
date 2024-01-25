43bfa4c38255b4bbf6c9630aa01b0433ef338697
jijiezhou <jijiez@email.unc.edu>
2024-01-25 18:26:24 -0500

    MdEditor

---
+++ zjoj-frontend/src/components/MdEditor.vue
@@ -0,0 +1,37 @@
+<template>
+  <Editor :value="value" :plugins="plugins" @change="handleChange" />
+</template>
+
+<script setup lang="ts">
+import gfm from "@bytemd/plugin-gfm";
+import highlight from "@bytemd/plugin-highlight";
+import { Editor, Viewer } from "@bytemd/vue-next";
+
+/**
+ * Define components attributes
+ */
+interface Props {
+  value: string;
+  mode?: string;
+  handleChange: (v: string) => void;
+}
+
+const plugins = [
+  gfm(),
+  highlight(),
+  // Add more plugins here
+];
+
+/**
+ * Set default values
+ */
+// eslint-disable-next-line no-undef
+const props = withDefaults(defineProps<Props>(), {
+  value: () => "",
+  handleChange: (v: string) => {
+    console.log(v);
+  },
+});
+</script>
+
+<style scoped></style>
