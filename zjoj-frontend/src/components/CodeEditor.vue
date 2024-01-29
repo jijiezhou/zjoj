5e7af24cb740a907043a0b76e2a4f035dd8978ad
jijiezhou <jijiez@email.unc.edu>
2024-01-29 13:49:07 -0500

    CodeEditor response with language change

--- zjoj-frontend/src/components/CodeEditor.vue
+++ zjoj-frontend/src/components/CodeEditor.vue
@@ -47,18 +47,12 @@ const value = ref("hello world");
 watch(
   () => props.language,
   () => {
-    alert(props.language);
-    codeEditor.value = monaco.editor.create(codeEditorRef.value, {
-      value: props.value,
-      language: props.language,
-      automaticLayout: true,
-      colorDecorators: true,
-      minimap: {
-        enabled: true,
-      },
-      readOnly: false,
-      theme: "vs-dark",
-    });
+    if (codeEditor.value) {
+      monaco.editor.setModelLanguage(
+        toRaw(codeEditor.value).getModel(),
+        props.language
+      );
+    }
   }
 );
 
