87f612169d3be34471aee950850c76e8a27ef056
jijiezhou <jijiez@email.unc.edu>
2024-02-09 14:57:29 -0500

    gateway config and filter

---
+++ zjoj-backend-microservice/zjoj-backend-gateway/src/main/java/com/zjj/zjojbackendgateway/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package com.zjj.zjojbackendgateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+import java.util.Arrays;
+
+/**
+ * @Classname CorsConfig
+ * @Description TODO
+ * @Author zjj
+ * @Date 2/9/24 2:19 PM
+ */
+// Resolve CORS
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+        // todo 实际改为线上真实域名、本地域名 deployment: change to real domain name
+        config.setAllowedOriginPatterns(Arrays.asList("*"));
+        config.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
+
---
+++ zjoj-backend-microservice/zjoj-backend-gateway/src/main/java/com/zjj/zjojbackendgateway/filter/GlobalAuthFilter.java
@@ -0,0 +1,54 @@
+package com.zjj.zjojbackendgateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @Classname GlobalAuthFilter
+ * @Description TODO
+ * @Author zjj
+ * @Date 2/9/24 2:23 PM
+ */
+@Component
+public class GlobalAuthFilter implements GlobalFilter, Ordered {
+    private AntPathMatcher antPathMatcher = new AntPathMatcher();
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        String path = request.getURI().getPath();
+
+        //check if path include inner, then intercept
+        if (antPathMatcher.match("/**/inner/**", path)) {
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.FORBIDDEN);
+            DataBufferFactory dataBufferFactory = response.bufferFactory();
+            DataBuffer dataBuffer = dataBufferFactory.wrap(String.valueOf("No authentication").getBytes(StandardCharsets.UTF_8));
+            return response.writeWith(Mono.just(dataBuffer));
+        }
+        //todo global authentication: jwt get login user info
+        //continue
+        return chain.filter(exchange);
+    }
+
+    /**
+     * highest priority
+     * @return
+     */
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
