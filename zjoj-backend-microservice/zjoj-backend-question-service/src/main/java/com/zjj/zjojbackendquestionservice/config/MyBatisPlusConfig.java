db2106b82d6e7ed590503abce4ed90ef0ce689c4
jijiezhou <jijiez@email.unc.edu>
2024-02-10 23:48:25 -0500

    2.10

---
+++ zjoj-backend-microservice/zjoj-backend-question-service/src/main/java/com/zjj/zjojbackendquestionservice/config/MyBatisPlusConfig.java
@@ -0,0 +1,31 @@
+package com.zjj.zjojbackendjudgeservice.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatis Plus Configuration
+ *
+ * @author https://github.com/liyupi
+ */
+@Configuration
+@MapperScan("com.zjj.zjojbackendjudgeservice")
+public class MyBatisPlusConfig {
+
+    /**
+     * Interceptor Configuration
+     *
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // pagination
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
