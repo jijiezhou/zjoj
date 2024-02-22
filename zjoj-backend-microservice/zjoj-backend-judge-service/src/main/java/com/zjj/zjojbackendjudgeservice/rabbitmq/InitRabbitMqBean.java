8e4ff7cdb56646b3e327ac330bbfcc527ce8c24e
jijiezhou <jijiez@email.unc.edu>
2024-02-21 23:56:22 -0500

    2.21

---
+++ zjoj-backend-microservice/zjoj-backend-judge-service/src/main/java/com/zjj/zjojbackendjudgeservice/rabbitmq/InitRabbitMqBean.java
@@ -0,0 +1,44 @@
+package com.zjj.zjojbackendjudgeservice.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @Classname InitRabbitMqBean
+ * @Description TODO
+ * @Author zjj
+ * @Date 2/18/24 4:34 PM
+ */
+
+@Slf4j
+@Component
+public class InitRabbitMqBean {
+    @Value("${spring.rabbitmq.host:localhost}")
+    private String host;
+
+    @PostConstruct
+    public void init() {
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost(host);
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            String EXCHANGE_NAME = "code_exchange";
+            channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+
+            // create queue
+            String queueName = "code_queue";
+            channel.queueDeclare(queueName, true, false, false, null);
+            channel.queueBind(queueName, EXCHANGE_NAME, "my_routingKey");
+            log.info("message queue start success");
+        } catch (Exception e) {
+            log.error("message queue start fail", e);
+        }
+    }
+}
